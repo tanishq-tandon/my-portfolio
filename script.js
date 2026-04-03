e6acdcc60f697fb5fbd7ed22b75bfee52707a97f
Tanishq Tandon <tanishqtandon10a@gmail.com>
2026-04-03 17:27:21 +0530

    Added responsive design for mobile device

---
+++ script.js
@@ -0,0 +1,10 @@
+let title = document.getElementById("main-title");
+
+// CHANGE its content
+title.innerHTML = "Tanishq Tandon 🚀";
+
+// Now let's make a button DO something
+function changeTitle() {
+    title.innerHTML = "Hire Me! 💼";
+    title.style.fontSize = "80px";
+}
